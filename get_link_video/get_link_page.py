c3c1d15004959cdb8d2a38d3513cfd1f3ce51fab
hataketsu <hataketsu@gmail.com>
2019-10-09 22:42:15 +0700

    a

---
+++ get_link_video/get_link_page.py
@@ -0,0 +1,55 @@
+import json
+import os
+import re
+
+import bs4
+import requests
+
+header_cookie = ""
+page_id = "sofunnysokyoot"
+VIDEOS_TXT = page_id + '_videos.txt'
+if os.path.isfile('cookies.txt'):
+    header_cookie = open('cookies.txt').read().strip()
+else:
+    print('No cookie')
+    input()
+    exit(1)
+session = requests.Session()
+if os.path.isfile(VIDEOS_TXT):
+    videos = [i.strip() for i in open(VIDEOS_TXT).readlines()]
+else:
+    videos = []
+
+
+def save_videos():
+    with open(VIDEOS_TXT, 'w') as fw:
+        fw.write('\n'.join(videos))
+
+
+session.headers = {
+    'authority': 'mbasic.facebook.com',
+    'cache-control': 'max-age=0',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+    'dnt': '1',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'accept-language': 'en-US,en;q=0.9,ja;q=0.8,ny;q=0.7',
+    'cookie': header_cookie
+}
+next_page_url = f"https://mbasic.facebook.com/{page_id}"
+while True:
+    page_content = session.get(next_page_url)
+    html = bs4.BeautifulSoup(page_content.text, "html.parser")
+
+    next_page_url = "https://mbasic.facebook.com"+html.select("#structured_composer_async_container > div:nth-child(2) >a")[0]['href']
+    for article in html.select("[role=article]"):
+        if r'video_redirect' in str(article) and "data-ft" in article.attrs:
+            data = json.loads(article['data-ft'])
+            post_id = data['mf_story_key']
+            video_url = f"https://www.facebook.com/{post_id}/"
+            if not video_url in videos:
+                print('New >>>', video_url)
+                videos.append(video_url)
+                save_videos()
+            else:
+                print('Old >>>', video_url)
